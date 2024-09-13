7bd1dc9edec43e509217cda48724a84c3d5def58
Giorgi Gavasheli <giorgi.gavasheli@shopboss.net>
2024-09-14 03:38:16 +0400

    test

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('index/Index');
-// });
 Route::get('/', function () {
+    return Inertia::render('index/Index');
+});
+Route::get('/test', function () {
     return 'Hello';
 });
